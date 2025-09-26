afc8171bffe2ffe64cd327d9ab7485479de2cd87
me_on_the-ROAD <168345837+01agx@users.noreply.github.com>
2025-09-26 22:19:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+/*     const age = 78
+if(age < 18)
+{ 
+    console.log('minor')
+}
+else if(age >= 18 && age < 60) 
+{
+    console.log('ure young')
+}
+else 
+{
+    console.log('old')
+}    */
+
+/*  const randomNumber = Math.random() * 10
+const greatNumber = Math.floor(randomNumber)
+console.log(greatNumber)     */
+
+/*   const test1 = true
+    const test2 = false 
+
+    if (test1 == false || test2 == true)
+    {
+        console.log("OK")
+    }
+    else{
+        console.log('NOT Ok')
+    }
+*/
+   
+    
+        /* or => ||
+        and => &&
+        */
+    // EXERCICE //
+    
+    /* const randomNumber = Math.random() * 10
+    console.log(Math.random(randomNumber)) */
+
+
+
+/*   function checkNumber(num) {
+    const randomNumber = Math.random() * 10
+const num = Math.floor(randomNumber)
+    console.log(num)
+
+    if(num == 0)
+    {
+    console.log('u won')
+    }
+    else if(num >= 2 || num == 3)
+    { 
+        console.log('ure more closer')
+    }
+    else if(num > 5)
+    {
+        console.log('ure so far')
+    }
+}
+
+checkNumber(5) */
+
+/*function happybirtday(username , age)
+{
+    console.log('happybirtday ' + username + ' you are '+ age + 'y old now!');
+}
+happybirtday('ameed' , 29); */
+
+
+
